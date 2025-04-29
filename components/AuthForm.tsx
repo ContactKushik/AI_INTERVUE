77b0b375cae3b613d5d1662cb48a5fe30bde2664
ContactKushik <127011605+ContactKushik@users.noreply.github.com>
2025-04-29 15:25:58 +0530

    fixed labels

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -94,10 +94,10 @@ const AuthForm = ({ type }: { type: FormType }) => {
             height={32}
             width={38}
           />
-          <h2 className="text-primary-100">PrepWise</h2>
+          <h2 className="text-primary-100">Intervue</h2>
         </div>
 
-        <h3>Practice job interview with AI</h3>
+        <h3>Ace your job interview with us</h3>
 
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-6 mt-4 form">
